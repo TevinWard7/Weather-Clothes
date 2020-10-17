e7e7c9c8aa41f0f207364290a2b3120910d65059
Tevin <tevinward7@gmail.com>
2020-10-16 21:07:27 -0400

    made template like layout for homepage

---
+++ src/components/Navbar/nav.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./nav.css"
+
+const Navbar = () => {
+    return(
+        <div className="my-container">
+            <div><p>gear box here</p></div>
+            <div id="date"><p>today's date</p></div>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/components/WeatherClothes/weatherclothes.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const WeatherClothes = () => {
+
+    return(
+        <div className="container">
+        <div className="row text-center">
+            <div className="col-12">
+                Weather Icon
+            </div>
+        </div>
+        <div className="row text-center">
+            <div className="col"></div>
+            <div className="col">
+                <ul>
+                    <li>clothes1 <span>name</span></li>
+                    <li>clothes1 <span>name</span></li>
+                    <li>clothes1 <span>name</span></li>
+                </ul>
+            </div>
+            <div className="col"></div>
+        </div>
+        </div>
+    )
+};
+
+export default WeatherClothes
