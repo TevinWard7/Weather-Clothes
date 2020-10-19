44684d595dc01989515426e9d36f0bcd3ee36f28
Tevin <tevinward7@gmail.com>
2020-10-18 22:59:32 -0400

    fleshed out add outfit page some

--- src/Pages/AddOutfit/addoutfit.js
+++ src/Pages/AddOutfit/addoutfit.js
@@ -4,31 +4,64 @@ import "./addoutfit.css"
 const AddOutfit = () => {
 
     return(
-        <div className="text-center">
+        <div className="row">
 
-            <ul className="clothing-inputs">
-                <li>
+            <div className="col"></div>
+
+            <div className="col">
+
+            <form className="clothing-inputs">
+
+                <label for="tops">Tops</label><br/>
+                <input type="text" name="top-name"></input>
+
+                <input type="file" name="top-image" id="fileToUpload"></input>
+                <input type="submit" value="Upload Image" name="submit"></input>
+
+                <br/>
+
+                <label for="bottoms">Bottoms</label><br/>    
+                <input type="text" name="bottoms"></input>
+
+                <input type="file" name="bottom-image" id="fileToUpload"></input>
+                <input type="submit" value="Upload Image" name="submit"></input>
+
+                <br/>
+
+                <label for="shoes">Shoes</label><br/>    
+                <input type="text" name="shoes"></input>
+
+                <input type="file" name="shoe-image" id="fileToUpload"></input>
+                <input type="submit" value="Upload Image" name="submit"></input>
+
+                <br/>
+
+                <br/>
+
                     <select>
                         <option>Casual</option>
                         <option>Work</option>
                         <option>Event</option>
                     </select>
-                </li>
-                <li>
+
                     <select>
                         <option>Hot</option>
                         <option>Cold</option>
                     </select>
-                </li>
-                <li>
+
                     <select>
+                        <option>Dry</option>
                         <option>Rain</option>
-                        <option>None</option>
                     </select>
-                </li>
-            </ul>
+            </form>
+            
+            </div>
+
+            <div className="col"></div>
 
         </div>
+
+        
     )
 
 };
