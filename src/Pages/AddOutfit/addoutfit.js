3afeceb93770d158bc2b852752e491d5c7371592
Tevin <tevinward7@gmail.com>
2020-10-24 02:19:28 -0400

    added navbar toggle and event listeners on inputs for add clothes page

--- src/Pages/AddOutfit/addoutfit.js
+++ src/Pages/AddOutfit/addoutfit.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./addoutfit.css"
 
 const AddOutfit = () => {
+
+    const [topName, setTopName] = useState();
+    const [bottomName, setBottomName] = useState();
+    const [shoesName, setShoesName] = useState();
+    console.log(topName)
 
     return(
         <div className="row">
@@ -12,16 +17,20 @@ const AddOutfit = () => {
 
             <form className="clothing-inputs">
 
-                <label for="tops">Tops</label><br/>
-                <input type="text" name="top-name"></input>
+                Outfit
+                <br/>
+                <br/>
+
+                <label for="tops">Top</label><br/>
+                <input type="text" name="top-name" onChange={(e) => setTopName(e.target.value)}></input>
 
                 <input type="file" name="top-image" id="fileToUpload"></input>
                 <input type="submit" value="Upload Image" name="submit"></input>
 
                 <br/>
 
-                <label for="bottoms">Bottoms</label><br/>    
-                <input type="text" name="bottoms"></input>
+                <label for="bottoms">Bottom</label><br/>    
+                <input type="text" name="bottoms" onChange={(e) => setBottomName(e.target.value)}></input>
 
                 <input type="file" name="bottom-image" id="fileToUpload"></input>
                 <input type="submit" value="Upload Image" name="submit"></input>
@@ -29,7 +38,7 @@ const AddOutfit = () => {
                 <br/>
 
                 <label for="shoes">Shoes</label><br/>    
-                <input type="text" name="shoes"></input>
+                <input type="text" name="shoes" onChange={(e) => setShoesName(e.target.value)}></input>
 
                 <input type="file" name="shoe-image" id="fileToUpload"></input>
                 <input type="submit" value="Upload Image" name="submit"></input>
@@ -45,6 +54,7 @@ const AddOutfit = () => {
                     </select>
 
                     <select>
+                        <option>Neutral</option>
                         <option>Hot</option>
                         <option>Cold</option>
                     </select>
--- src/components/Navbar/nav.js
+++ src/components/Navbar/nav.js
@@ -1,13 +1,42 @@
-import React from "react"
-import "./nav.css"
+import React, { useState } from "react";
+import "./nav.css";
+import SettingsSharpIcon from '@material-ui/icons/SettingsSharp';
+import { IconButton } from "@material-ui/core";
 
 const Navbar = () => {
+
+    // Determine if nav pop is open/closed
+    const [navActive, setNavActive] = useState("false");
+    // Determine annimation
+    const [fadeIn, setFadeIn] = useState(0);
+
+    // Toggle Our Navigation Bar
+    const toggleNav = () => {
+        if (navActive === "true") {
+            setNavActive("false")
+            setFadeIn(0)
+        } else {
+            setNavActive("true")
+            setFadeIn(1)
+        }
+    };
+
     return(
         <div className="my-container">
-            <div><p>gear box here</p></div>
+            <nav>
+            <div><IconButton id="gear" onClick={toggleNav}><SettingsSharpIcon /></IconButton></div>
+            
+                <ul className={`nav-links ${navActive === "true" ? "nav-active" : ""}`} fadein={fadeIn} onAnimationEnd={() => {setFadeIn(0)}}>
+                    <li><a href="/">Home</a></li>
+                    <li><a href>Wardrobe</a></li>
+                    <li><a href="/add">Add Outfit</a></li>
+                </ul>
+                
+            </nav>
+            
             <div id="date"><p>today's date</p></div>
         </div>
     )
 }
 
-export default Navbar
+export default Navbar;
